16b9fcf326c0838bba8a86a4a2cf89afe167a06d
Yonatan Zewude <yonatanzewude@hotmail.com>
2024-02-27 07:51:40 +0100

    Added Animal component with functionality to display animal details, handle image loading and errors, and make a function  if the animal needs to be fed.

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { IAnimalSmall } from "../models/IAnimalSsmall";
+import { getAnimals } from "../services/animalService";
+import { Animal } from "../components/Animal";
 import "../style/main.scss";
 
 export const Animals = () => {
@@ -12,3 +16,27 @@ export const Animals = () => {
                 console.error("Error fetching animals data:", error);
             }
         };
+
+        if (animals.length === 0) {
+            fetchData();
+        }
+    }, [animals]);
+
+    useEffect(() => {
+        const localStorageAnimals = JSON.parse(localStorage.getItem("Animals") || "[]");
+        if (localStorageAnimals.length === 0 && animals.length > 0) {
+            localStorage.setItem("Animals", JSON.stringify(animals));
+        }
+    }, [animals]);
+
+    const animalComponents = animals.map((animal) => (
+        <Animal animalData={animal} key={animal.id} />
+    ));
+
+    return (
+        <>
+            <h1 className="title"> Welcome To WildWonder Zoo</h1>
+            <div className="animals">{animalComponents}</div>
+        </>
+    );
+};
